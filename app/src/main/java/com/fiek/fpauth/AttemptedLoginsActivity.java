966744874f714a4bc34089fa2a5136632335b3ea
Elon Demi <elon.demi@student.uni-pr.edu>
2023-06-22 23:36:17 +0200

    Fixed an error

---
+++ app/src/main/java/com/fiek/fpauth/AttemptedLoginsActivity.java
@@ -0,0 +1,14 @@
+package com.fiek.fpauth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AttemptedLoginsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_attempted_logins);
+    }
+}
--- app/src/main/java/com/fiek/fpauth/FPAuthActivity.java
+++ app/src/main/java/com/fiek/fpauth/FPAuthActivity.java
@@ -1,6 +1,11 @@
 package com.fiek.fpauth;
 
 
+
+
+import static androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG;
+import static androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.biometric.BiometricManager;
@@ -8,23 +13,20 @@ import androidx.biometric.BiometricPrompt;
 import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
+
 import android.provider.Settings;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.concurrent.Executor;
 
-public class MainActivity extends AppCompatActivity {
+public class FPAuthActivity extends AppCompatActivity {
 
 
-
-
+    private static final int REQUEST_CODE = 0000;
     private Executor executor;
     private BiometricPrompt biometricPrompt;
     private BiometricPrompt.PromptInfo promptInfo;
@@ -38,26 +40,29 @@ public class MainActivity extends AppCompatActivity {
         Button login_btn = findViewById(R.id.login_btn);
 
 
+
         BiometricManager biometricManager = BiometricManager.from(this);
-        switch (biometricManager.canAuthenticate()) {
+        switch (biometricManager.canAuthenticate(BIOMETRIC_STRONG | DEVICE_CREDENTIAL)) {
             case BiometricManager.BIOMETRIC_SUCCESS:
-                msg_txt.setText("You can use the fingerprint sensor to login");
-                msg_txt.setTextColor(Color.parseColor("#fafafa"));
+                Log.d("MY_APP_TAG", "App can authenticate using biometrics.");
                 break;
             case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
-                msg_txt.setText("The device doesn't have a fingerprint sensor");
+                Toast.makeText(this, "FingerPrint sensor Not exist", Toast.LENGTH_SHORT).show();
                 break;
             case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
-                msg_txt.setText("The biometric sensor is currnetly unavailable");
-                login_btn.setVisibility(View.GONE);
+                Toast.makeText(this, "Sensor not available or busy", Toast.LENGTH_LONG).show();
+                break;
             case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
-                msg_txt.setText("Your device doesn't jave any fingerprint saved, please check you security settings");
-                login_btn.setVisibility(View.GONE);
+                // Prompts the user to create credentials that your app accepts.
+                final Intent enrollIntent = new Intent(Settings.ACTION_BIOMETRIC_ENROLL);
+                enrollIntent.putExtra(Settings.EXTRA_BIOMETRIC_AUTHENTICATORS_ALLOWED,
+                        BIOMETRIC_STRONG | DEVICE_CREDENTIAL);
+                startActivityForResult(enrollIntent, REQUEST_CODE);
                 break;
         }
 
         executor = ContextCompat.getMainExecutor(this);
-        BiometricPrompt biometricPrompt = new BiometricPrompt(MainActivity.this,
+        biometricPrompt  = new BiometricPrompt(FPAuthActivity.this,
                 executor, new BiometricPrompt.AuthenticationCallback() {
             @Override
             public void onAuthenticationError(int errorCode,
@@ -72,7 +77,7 @@ public class MainActivity extends AppCompatActivity {
             public void onAuthenticationSucceeded(
                     @NonNull BiometricPrompt.AuthenticationResult result) {
                 super.onAuthenticationSucceeded(result);
-                startActivity(new Intent(MainActivity.this, HomeActivity.class));
+                startActivity(new Intent(FPAuthActivity.this, HomeActivity.class));
                 Toast.makeText(getApplicationContext(),
                         "Authentication succeeded!", Toast.LENGTH_SHORT).show();
             }
@@ -86,18 +91,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        BiometricPrompt.PromptInfo promptInfo = new BiometricPrompt.PromptInfo.Builder()
+        promptInfo = new BiometricPrompt.PromptInfo.Builder()
                 .setTitle("Biometric login ")
                 .setSubtitle("Log in using your biometric credential")
                 .setNegativeButtonText("Use account password")
                 .build();
 
 
-        login_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-                    public void onClick(View view){
-                    biometricPrompt.authenticate(promptInfo);
-            }
+        login_btn.setOnClickListener(view -> {
+            biometricPrompt.authenticate(promptInfo);
         });
     }
 
