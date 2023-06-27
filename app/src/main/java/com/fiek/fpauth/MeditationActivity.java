28bd0e15bc43618f31d3c105b573f3c25ef9d348
Elon Demi <elon.demi@student.uni-pr.edu>
2023-06-27 19:09:35 +0200

    Added meditation activity

---
+++ app/src/main/java/com/fiek/fpauth/MeditationActivity.java
@@ -0,0 +1,14 @@
+package com.fiek.fpauth;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MeditationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meditation);
+    }
+}
